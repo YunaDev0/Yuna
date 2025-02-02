486ff1ec6792cba3606da307efd32241394937e0
overweight-potato <overweightpotato8@gmail.com>
2025-02-02 15:30:52 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,14 @@
+local GameID = game.GameId
 local Exploit = identifyexecutor()
 
-local gameIds = {
+local Player = game:GetService("Players").LocalPlayer
+local LogService = game:GetService("LogService")
+local ScriptContext = game:GetService("ScriptContext")
+
+-- // If you're buying yuna then you should prob buy a paid exploit, not just buy only yuna
+local Shitsploits = { "Solara", "Xeno" }
+
+local GameIDs = {
     ["3150475059"] = "https://api.luarmor.net/files/v3/loaders/295c9f0edac4d95d36fb9defe521ca87.lua", -- FF2
     ["115797356"] = "https://api.luarmor.net/files/v3/loaders/b3f78af212d54a1d008b143aac4a86be.lua",
     ["3032132418"] = "https://api.luarmor.net/files/v3/loaders/b1cd6d05e9596d7b36b423139f5433e3.lua", -- HCBB
@@ -8,24 +16,57 @@ local gameIds = {
     ["184199275"] = "https://api.luarmor.net/files/v3/loaders/a553b5c3e7ff1378e91c7f7af656fb98.lua", -- UF
 }
 
-local solaraIds = {
+local SolaraIDs = {
     ["3150475059"] = "https://api.luarmor.net/files/v3/loaders/9292f827569f2535f2c40a0ba7c42deb.lua", -- FF2
     ["3032132418"] = "https://api.luarmor.net/files/v3/loaders/b1cd6d05e9596d7b36b423139f5433e3.lua" -- HCBB
 }
 
---if Exploit == "Nihon" then
-    --game:GetService("Players").LocalPlayer:Kick("Yuna | Nihon is currently unable to support Yuna.")
-    --return
---end
+local function Execute(IDs)
+    return loadstring(game:HttpGet(IDs[tostring(GameID)]))()
+end
 
-if Exploit == "Solara" then
-    if not solaraIds[tostring(game.GameId)] then
-        game:GetService("Players").LocalPlayer:Kick("Yuna | Solara is currently unable to support this game.")
-        return
+-- // Key detection (more detailed kick message because LRM's is not that great)
+if not script_key then
+    return Player:Kick("Yuna | Please make sure you include the script_key part ABOVE the loadstring, otherwise Luarmor will not be able to recognize that you bought.")
+end
+
+-- // LogService/ScriptContext error detection bypass
+if GameID == 3150475059 and not table.find(Shitsploits, Exploit) then
+    if hookfunction then
+        local Old; Old = hookfunction(LogService.GetLogHistory, function(...)
+            local Results = Old(...);
+
+            -- // Remove any potential errors that the game could detect, by default the console has 3 errors in it already
+            if #Results > 3 then
+                for i = 4, #Results do
+                    if type(Results[i]) == "table" and Results[i].messageType == Enum.MessageType.MessageError then
+                        table.remove(Results, i);
+                    end
+                end
+            end
+
+            return Results
+        end)
     end
-    loadstring(game:HttpGet(solaraIds[tostring(game.GameId)]))()
+
+    if getconnections then
+        for i,v in next, getconnections(ScriptContext.Error) do
+            if v.Function then
+                v:Disable()
+            end
+        end
+    end
+end
+
+
+if table.find(Shitsploits, Exploit) then
+    if not SolaraIDs[tostring(GameID)] then
+        return Player:Kick("Yuna | Solara is currently unable to support this game.")
+    end
+
+    Execute(SolaraIDs)
 elseif Exploit == "Fluxus" then
-    game:GetService("Players").LocalPlayer:Kick("Yuna | For mobile please use Codex or Arceus.")
-elseif gameIds[tostring(game.GameId)] then
-    loadstring(game:HttpGet(gameIds[tostring(game.GameId)]))()
+    Player:Kick("Yuna | For mobile please use Codex or Arceus.")
+elseif GameIDs[tostring(GameID)] then
+    Execute(GameIDs)
 end
